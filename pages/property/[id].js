b10be3aff01ab0a118c13496182dcbec3b676c89
IsmailEll <34143618+IsmailEll@users.noreply.github.com>
2022-03-12 12:09:59 +0100

    Update [id].js
    Added a button to expand or shorten the description.

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,4 +1,5 @@
-import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
+import {useState} from "react";
+import { Box, Flex, Spacer, Text, Button } from '@chakra-ui/layout';
 import { Avatar } from '@chakra-ui/avatar';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
@@ -8,56 +9,66 @@ import millify from 'millify';
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 import ImageScrollbar from '../../components/ImageScrollbar';
 
-const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
-  <Box maxWidth='1000px' margin='auto' p='4'>
-    {photos && <ImageScrollbar data={photos} />}
-    <Box w='full' p='6'>
-      <Flex paddingTop='2' alignItems='center'>
-        <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
-        <Text fontWeight='bold' fontSize='lg'>
-          AED {price} {rentFrequency && `/${rentFrequency}`}
+const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => {
+ 
+  const [viewFullDescription, setViewFullDescription] = useState(false);
+  
+  return (
+    <Box maxWidth='1000px' margin='auto' p='4'>
+      {photos && <ImageScrollbar data={photos} />}
+      <Box w='full' p='6'>
+        <Flex paddingTop='2' alignItems='center'>
+          <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
+          <Text fontWeight='bold' fontSize='lg'>
+            AED {price} {rentFrequency && `/${rentFrequency}`}
+          </Text>
+          <Spacer />
+          <Avatar size='sm' src={agency?.logo?.url}></Avatar>
+        </Flex>
+        <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+          {rooms}<FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+      </Box>
+      <Box marginTop='2'>
+        <Text fontSize='lg' marginBottom='2' fontWeight='bold'>{title}</Text> 
+        <Text lineHeight='2' color='gray.600' cursor="pointer" >
+            {viewFullDescription ? description : `${description.substring(0, 300)}...`}
+            <Button fontSize="light" marginLeft="1.5" p="3" lineHeight={1} onClick={() => setViewFullDescription(prevState => !prevState)}>
+                    {viewFullDescription ? 'shorten' : 'expand'}
+            </Button>
         </Text>
-        <Spacer />
-        <Avatar size='sm' src={agency?.logo?.url}></Avatar>
-      </Flex>
-      <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
-        {rooms}<FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
-      </Flex>
-    </Box>
-    <Box marginTop='2'>
-      <Text fontSize='lg' marginBottom='2' fontWeight='bold'>{title}</Text>
-      <Text lineHeight='2' color='gray.600'>{description}</Text>
-    </Box>
-    <Flex flexWrap='wrap' textTransform='uppercase' justifyContent='space-between'>
-      <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3'>
-        <Text>Type</Text>
-        <Text fontWeight='bold'>{type}</Text>
-      </Flex>
-      <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3'>
-        <Text>Purpose</Text>
-        <Text fontWeight='bold'>{purpose}</Text>
-      </Flex>
-      {furnishingStatus && (
-        <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3' >
-          <Text>Furnishing Status</Text>
-          <Text fontWeight='bold'>{furnishingStatus}</Text>
+      </Box>
+      <Flex flexWrap='wrap' textTransform='uppercase' justifyContent='space-between'>
+        <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3'>
+          <Text>Type</Text>
+          <Text fontWeight='bold'>{type}</Text>
         </Flex>
-      )}
-    </Flex>
-    <Box>
-      {amenities.length && <Text fontSize='2xl' fontWeight='black' marginTop='5'>Facilites:</Text>}
-        <Flex flexWrap='wrap'>
-          {amenities?.map((item) => (
-              item?.amenities?.map((amenity) => (
-                <Text key={amenity.text} fontWeight='bold' color='blue.400' fontSize='l' p='2' bg='gray.200' m='1' borderRadius='5'>
-                  {amenity.text}
-                </Text>
-              ))
-          ))}
+        <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3'>
+          <Text>Purpose</Text>
+          <Text fontWeight='bold'>{purpose}</Text>
         </Flex>
+        {furnishingStatus && (
+          <Flex justifyContent='space-between' w='400px' borderBottom='1px' borderColor='gray.100' p='3' >
+            <Text>Furnishing Status</Text>
+            <Text fontWeight='bold'>{furnishingStatus}</Text>
+          </Flex>
+        )}
+      </Flex>
+      <Box>
+        {amenities.length && <Text fontSize='2xl' fontWeight='black' marginTop='5'>Facilites:</Text>}
+          <Flex flexWrap='wrap'>
+            {amenities?.map((item) => (
+                item?.amenities?.map((amenity) => (
+                  <Text key={amenity.text} fontWeight='bold' color='blue.400' fontSize='l' p='2' bg='gray.200' m='1' borderRadius='5'>
+                    {amenity.text}
+                  </Text>
+                ))
+            ))}
+          </Flex>
+      </Box>
     </Box>
-  </Box>
-);
+  )
+};
 
 export default PropertyDetails;
 
