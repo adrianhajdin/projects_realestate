b52fd8611707515a82ea7c9158fa5e9fdfc364e8
BRIDGE-LTD <kaneda213@gmail.com>
2023-01-24 02:38:52 +0900

    APP 起動

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,9 +6,11 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      // 'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': '22f8a96c7dmsh1b748b2a386a8ccp1a2e9fjsnbdb85cfabb6e',
+
     },
   });
-    
+
   return data;
 }
