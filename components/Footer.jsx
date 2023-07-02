399f50dd1a93d9916f62bf5bcf8744f7265eab0c
blsreda <redx.freelancer@gmail.com>
2023-07-02 12:17:06 +0200

    edit the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
-    © 2021 Realtor, Inc.
+    © 2023 Realtor, Inc.
   </Box>
 );
 
